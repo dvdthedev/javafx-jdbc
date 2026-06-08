6dfc518b32fb54dc69473fbd72edb2a7e8b19f0c
dvdthedev <deividzor@gmail.com>
2026-06-08 12:00:28 -0300

    main view design

--- src/main/java/com/example/javafxjdbc/HelloApplication.java
+++ src/main/java/com/example/javafxjdbc/HelloApplication.java
@@ -3,6 +3,7 @@ package com.example.javafxjdbc;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.ScrollPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -11,7 +12,15 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("MainView.fxml"));
-        Scene scene = new Scene(fxmlLoader.load());
+
+        //setando uma instância de scrollpane para ser a tela principal da app
+        ScrollPane scrollPane = fxmlLoader.load();
+
+        //definições para preenchimento da barra utilizando scrollpane
+        scrollPane.setFitToHeight(true);
+        scrollPane.setFitToWidth(true);
+
+        Scene scene = new Scene(scrollPane);
         stage.setTitle("Sample JAVAFX App");
         stage.setScene(scene);
         stage.show();
